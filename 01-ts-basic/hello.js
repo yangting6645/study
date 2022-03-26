bc5bf3b36d7bea48a60f1a543543fcc02b721ace
yangting <2913483760@qq.com>
2022-03-26 22:04:07 +0800

    hello

---
+++ 01-ts-basic/hello.js
@@ -0,0 +1,12 @@
+//你好，世界
+//console.log('Hello,World')
+
+function greet(person:string,date:Date) {
+    console.log('Hello${person},today is ${data}.')
+}
+
+greet('小于',new Date());ts
+
+//let msg = 'hello there!'
+//msg = 'hello world'
+//msg = 100
